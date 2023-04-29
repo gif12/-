e119306fef870869fd4fff21e426711f681e8eda
gif12 <91024081+gif12@users.noreply.github.com>
2023-04-29 22:09:35 +0330

    Update process.php

--- process.php
+++ process.php
@@ -1,53 +1,111 @@
-$servername = "localhost";
-$username = "[ali]";
-$password = "[1qaz]";
-$dbname = "[ali]";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+<?php
+// تنظیمات فایل و زمان
+date_default_timezone_set('Asia/Tehran');
+define('APP_PATH', dirname(__FILE__));
+define('DATA_PATH', APP_PATH . '/data');
+define('CONFIG_FILE', DATA_PATH . '/config.txt');
+define('USERS_FILE', DATA_PATH . '/users.txt');
+define('LOG_FILE', DATA_PATH . '/log.txt');
 
-// ثبت نام کاربران
-if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['register'])) {
-  $username = $_POST['username'];
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-
-  // بررسی تکراری نبودن ایمیل کاربر
-  $query = "SELECT * FROM users WHERE email='$email'";
-  $result = mysqli_query($conn, $query);
-  if (mysqli_num_rows($result) > 0) {
-    echo "این ایمیل قبلاً در سیستم ثبت شده است.";
-    exit();
-  }
-
-  $query = "INSERT INTO users (username, email, password) VALUES('$username', '$email', '$password')";
-  mysqli_query($conn, $query);
-
-  echo "ثبت نام با موفقیت انجام شد.";
+// تابع بررسی وجود پوشه و فایل‌ها
+function check_folders_files() {
+    if(!is_dir(DATA_PATH)) {
+        mkdir(DATA_PATH);
+        chmod(DATA_PATH, 0755);
+    }
+    if(!file_exists(CONFIG_FILE)) {
+        $fp = fopen(CONFIG_FILE, 'w');
+        fclose($fp);
+        chmod(CONFIG_FILE, 0600);
+    }
+    if(!file_exists(USERS_FILE)) {
+        $fp = fopen(USERS_FILE, 'w');
+        fclose($fp);
+        chmod(USERS_FILE, 0600);
+    }
+    if(!file_exists(LOG_FILE)) {
+        $fp = fopen(LOG_FILE, 'w');
+        fclose($fp);
+        chmod(LOG_FILE, 0600);
+    }
 }
 
-// ورود به حساب کاربری
-if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['login'])) {
-  $email = $_POST['email'];
-  $password = $_POST['password'];
+// بررسی وجود پوشه‌ها و فایل‌ها
+check_folders_files();
 
-  $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
-  $result = mysqli_query($conn, $query);
+// خواندن تنظیمات اتصال به دیتابیس
+$db_config = parse_ini_file(CONFIG_FILE);
 
-  if (mysqli_num_rows($result) > 0) {
-    // پیدا کردن نام کاربری با ایمیل
-    $user = mysqli_fetch_assoc($result);
-    $username = $user['username'];
+// اتصال به پایگاه داده
+$conn = mysqli_connect($db_config['host'], $db_config['username'], $db_config['password'], $db_config['dbname']);
 
-    // ذخیره اطلاعات کاربر در سطح جلسه
-    session_start();
-    $_SESSION['username'] = $username;
-    $_SESSION['email'] = $email;
-
-    echo "شما وارد حساب کاربری خود شدید.";
-  } else {
-    echo "ایمیل یا رمز عبور اشتباه است.";
-  }
+// بررسی وضعیت اتصال
+if(!$conn) {
+    die("اتصال به پایگاه داده با خطا مواجه شد.");
 }
 
-// بستن اتصال به دیتابیس
-mysqli_close($conn);
-.
+// شروع نشست
+session_start();
+
+// تنظیم متغیر خطا
+$error = '';
+
+// پردازش فرم‌ها
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    switch($_POST['form']) {
+        case 'register':
+            // ثبت‌نام کاربر جدید
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $confirm_password = $_POST['confirm_password'];
+            $email = $_POST['email'];
+
+            // بررسی تکراری نبودن ایمیل
+            $existing_users = file(USERS_FILE, FILE_IGNORE_NEW_LINES);
+            foreach($existing_users as $existing_user) {
+                $user_data = explode("|", $existing_user);
+                if($user_data[2] === $email) {
+                    $error = "این ایمیل قبلا در سیستم ثبت شده است.";
+                    break;
+                }
+            }
+
+            // ثبت‌نام کاربر جدید
+            if(empty($error) && strlen($username) >= 4 && strlen($password) >= 6 && $password === $confirm_password && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $password_hash = password_hash($password, PASSWORD_DEFAULT);
+                $new_user = "$username|$password_hash|$email\n";
+                file_put_contents(USERS_FILE, $new_user, FILE_APPEND);
+                header("Location: welcome.php");
+                exit;
+            } else {
+                $error = "فرم را با دقت پر کنید.";
+            }
+
+            break;
+        case 'login':
+            // ورود کاربر
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $existing_users = file(USERS_FILE, FILE_IGNORE_NEW_LINES);
+            $user_found = false;
+
+            // بررسی وجود کاربر
+            foreach($existing_users as $existing_user) {
+                $user_data = explode("|", $existing_user);
+                if($user_data[2] === $email && password_verify($password, $user_data[1])) {
+                    $user_found = true;
+                    break;
+                }
+            }
+
+            if($user_found) {
+                header("Location: welcome.php");
+                exit;
+            } else {
+                $error = "ایمیل یا رمز عبور اشتباه است.";
+            }
+
+            break;
+    }
+}
+?>
